6be5e72154db921b000b61720628560085bd7593
andres-donoso <andres.donoso51@gmail.com>
2022-01-20 02:27:00 +0100

    Removed comments.

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import chalk from 'chalk';
 import randomColor from 'randomcolor'; // imports the scripts
 
-// FIXME: Is this broken when the user doesn't pass the hue and luminosity?
-// const randomHexColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-
-// generates a random color -> e.g.: #56eec7
-
 // console.log('#'.repeat(31)); // repeats the character 31 times, helps instead of typing it out
 
 const color = randomColor({
